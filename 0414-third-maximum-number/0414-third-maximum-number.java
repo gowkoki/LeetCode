1457a2aeece998f0ab37ac1bf333936439716136
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-06-13 13:31:13 +0100

    Time: 4 ms (63.75%), Space: 43.6 MB (43.64%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,19 +1,24 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
 
-        for (int n : nums) {
-            set.add(n);
+        for (Integer n : nums) {
+            if (n.equals(max1) || n.equals(max2) || n.equals(max3))
+                continue;
+            if (max1 == null || n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (max2 == null || n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (max3 == null || n > max3) {
+                max3 = n;
+            }
         }
-
-        if (set.size() < 3) {
-            return Collections.max(set);
-        }
-
-        set.remove(Collections.max(set));
-        set.remove(Collections.max(set));
-
-        return Collections.max(set);
+        return max3 == null ? max1 : max3;
 
     }
 }
