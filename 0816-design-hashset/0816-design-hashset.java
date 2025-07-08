bdfe2e9686a78d7953df444fc07cf3659b2f475a
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-07-08 14:32:37 +0100

    Time: 616 ms (6.68%), Space: 51.4 MB (48.59%) - LeetHub

---
+++ 0816-design-hashset/0816-design-hashset.java
@@ -0,0 +1,31 @@
+class MyHashSet {
+    List<Integer> set;
+
+    public MyHashSet() {
+        set = new ArrayList<>();
+    }
+
+    public void add(int key) {
+        if (!contains(key)) {
+            set.add(key);
+        }
+    }
+
+    public void remove(int key) {
+        if (contains(key)) {
+            set.remove(set.indexOf(key));
+        }
+    }
+
+    public boolean contains(int key) {
+        return set.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
