c91a910f7ff4b26a99fa045c385d960b89791556
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-07-09 11:28:22 +0100

    Time: 33 ms (29.96%), Space: 55.2 MB (32.94%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -1,54 +1,22 @@
 class MyHashMap {
-    private static final int SIZE = 1000;
-    private List<int[]>[] map;
+    private final int[] map;
 
     public MyHashMap() {
-        map = new List[SIZE];
-
-        for (int i = 0; i < SIZE; i++) {
-            map[i] = new ArrayList<>();
-        }
-    }
-
-    private int getIndex(int key) {
-        return Integer.hashCode(key) % SIZE;
+        map = new int[1_000_001];
+        Arrays.fill(map, -1);
     }
 
     public void put(int key, int value) {
-        int idx = getIndex(key);
-
-        for (int[] pair : map[idx]) {
-            if (pair[0] == key) {
-                pair[1] = value;
-                return;
-            }
-        }
-
-        map[idx].add(new int[] { key, value });
+        map[key] = value;
 
     }
 
     public int get(int key) {
-        int idx = getIndex(key);
-
-        for (int[] pair : map[idx]) {
-            if (pair[0] == key) {
-                return pair[1];
-            }
-        }
-        return -1;
+        return map[key];
     }
 
     public void remove(int key) {
-        int idx = getIndex(key);
-        Iterator<int[]> it = map[idx].iterator();
-
-        while (it.hasNext()) {
-            if (it.next()[0] == key) {
-                it.remove();
-                return;
-            }
-        }
+        map[key] = -1;
     }
 }
 
