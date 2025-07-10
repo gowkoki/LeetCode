5ea5de046367076d07ca4d5372c2b6ef9cdd8bb5
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-07-10 10:34:57 +0100

    Time: 1 ms (99.89%), Space: 46.2 MB (60.08%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int output =0;
+
+        for(int n : nums){
+            output ^= n;
+        }
+
+        return output;
+    }
+}
