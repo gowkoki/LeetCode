29c77f2b9602baa454bcb8eedfe728243f3b75cc
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-07-15 09:58:21 +0100

    Time: 6 ms (98.27%), Space: 48.1 MB (44.4%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,6 +1,5 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
         Map<String, List<String>> anagram = new HashMap<>();
 
         for (String s : strs) {
@@ -9,16 +8,12 @@ class Solution {
             String sorted = String.valueOf(c);
 
             if (!anagram.containsKey(sorted)) {
-                List<String> list = new ArrayList<>();
-                list.add(s);
-                anagram.put(sorted, list);
-            } else {
-                anagram.get(sorted).add(s);
+                anagram.put(sorted, new ArrayList<>());
             }
+            anagram.get(sorted).add(s);
+
         }
 
-        result.addAll(anagram.values());
-
-        return result;
+        return new ArrayList<>(anagram.values());
     }
 }
