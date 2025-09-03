f153af264821b022538abdbc55e07649b64084e6
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-09-03 16:42:50 +0100

    Time: 1 ms (98.9%), Space: 44.4 MB (98.75%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,22 +1,17 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int[] result = new int[arr.length];
+        int[] res = new int[arr.length];
         int index = 0;
 
-        for (int i = 0; i < arr.length && index < result.length; i++) {
-            if (arr[i] == 0) {
-                result[index++] = arr[i];
-                if (index < result.length) {
-                    result[index++] = 0;
-                }
-            } else {
-                result[index++] = arr[i];
+        for (int i = 0; i < arr.length && index < res.length; i++) {
+            res[index++] = arr[i];
+            if (arr[i] == 0 && index < res.length) {
+                res[index++] = 0;
             }
-
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = result[i];
+        for (int i = 0; i < res.length; i++) {
+            arr[i] = res[i];
         }
     }
 }
