21ee113fc35f9c9fed1e63d8aea7d51108feec5b
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-10-14 14:56:56 +0100

    Time: 2 ms (98.98%), Space: 45.6 MB (5.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,13 +4,13 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
-
             if (map.containsKey(diff)) {
                 return new int[] { map.get(diff), i };
             }
-
             map.put(nums[i], i);
         }
+
         return new int[] {};
+
     }
 }
