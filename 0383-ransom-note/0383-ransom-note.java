289e0ac5e2ec23638df5180be7787997683ff49b
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-10-14 14:17:17 +0100

    Time: 11 ms (34.61%), Space: 45.1 MB (49.98%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> ran = new HashMap<>();
-
-        for (char c : ransomNote.toCharArray()) {
-            ran.put(c, ran.getOrDefault(c, 0) + 1);
-        }
+        Map<Character, Integer> mMap = new HashMap<>();
 
         for (char c : magazine.toCharArray()) {
-            if (ran.containsKey(c)) {
-                if (ran.get(c) > 1) {
-                    ran.put(c, ran.get(c) - 1);
-                } else {
-                    ran.remove(c);
-                }
-            }
-
-            if (ran.isEmpty()) {
-                return true;
-            }
+            mMap.put(c, mMap.getOrDefault(c, 0) + 1);
         }
 
-        return false;
+        for (char c : ransomNote.toCharArray()) {
+            if (mMap.containsKey(c)) {
+                if (mMap.get(c) == 1) {
+                    mMap.remove(c);
+                } else {
+                    mMap.put(c, mMap.get(c) - 1);
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
